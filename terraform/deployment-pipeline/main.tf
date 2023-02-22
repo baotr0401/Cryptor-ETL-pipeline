5b0f1d1854619cf522358b26f84ab37a505d1e04
baotr0401 <bao.tr0401@gmail.com>
2023-02-22 23:30:10 +0700

    code change

--- terraform/deployment-pipeline/main.tf
+++ terraform/deployment-pipeline/main.tf
@@ -14,24 +14,24 @@ provider "aws" {
 }
 
 module "S3_bucket_source" {
-    source = "../modules/S3"
-    bucket_name= var.bucket_list[0]
-    acl =   var.access_control_s3_bucket
+  source = "../modules/S3"
+  bucket_name= var.bucket_list[0]
+  acl =   var.access_control_s3_bucket
 
 
 }
 
 module "S3_bucket_destination" {
-    source = "../modules/S3"
-    bucket_name = var.bucket_list[1]
-    acl =   var.access_control_s3_bucket
+  source = "../modules/S3"
+  bucket_name = var.bucket_list[1]
+  acl =   var.access_control_s3_bucket
 
 
 }
 module "S3_bucket_checkpoint" {
-    source = "../modules/S3"
-    bucket_name= var.bucket_list[2]
-    acl =   var.access_control_s3_bucket
+  source = "../modules/S3"
+  bucket_name= var.bucket_list[2]
+  acl =   var.access_control_s3_bucket
 }
 
 # deploy glue script from local to "glue-job-scripts-bitcoin" S3 bucket
@@ -74,81 +74,81 @@ module "ec2_kafka_broker_instance"{
 
 
 module "IAM_glue_role" {
-    source = "../modules/IAM/iam_glue"
-    role_name= var.glue_role_name
-    policy_name = var.glue_policy_name
+  source = "../modules/IAM/iam_glue"
+  role_name= var.glue_role_name
+  policy_name = var.glue_policy_name
 }
 
 module "glue_job_1"{
-    source = "../modules/Glue/glue-job"
-    name = var.glue_names
-    glue_iam_role= module.IAM_glue_role.role_arn
-    worker_numbers = 10
-    worker_types = "G.1X"
-    glue_versions = "3.0"
+  source = "../modules/Glue/glue-job"
+  name = var.glue_names
+  glue_iam_role= module.IAM_glue_role.role_arn
 
-    bucket_source = module.S3_bucket_source.bucket_name
-    script_name = var.glue_script_key
- 
-    arguments = {
+  worker_numbers = 10
+  worker_types = "G.1X"
+  glue_versions = "3.0"
+  glue_type = var.glue_types
+
+  bucket_source = module.S3_bucket_source.bucket_name
+  script_name = var.glue_script_key
+  
+  arguments = {
         "S3_OUTPUT" = module.S3_bucket_destination.bucket_name
         "S3_CHECKPOINT" = module.S3_bucket_checkpoint.bucket_name
         "BOOTSTRAP_SERVER" = "${module.ec2_kafka_broker_instance.public_dns_hostname}:9093"
         "TOPIC" = var.kafka_topic
-
-    
-    }
-    glue_type = var.glue_types
+      }
+  
 }
 
 module "glue_trigger_job_1"{
-    source = "../modules/Glue/glue-trigger"
-    name = var.glue_trigger_name
-    cron_expressions = var.cron_expression
-    trigger_type = var.glue_trigger_type 
-    glue_job_name = module.glue_job_1.job_name
+  source = "../modules/Glue/glue-trigger"
+  name = var.glue_trigger_name
+  cron_expressions = var.cron_expression
+  trigger_type = var.glue_trigger_type 
+  glue_job_name = module.glue_job_1.job_name
 }
 
 # Lambda
 
 module "IAM_lambda_role" {
-    source = "../modules/IAM/iam_lambda"
-    role_name= var.lambda_role_name
-    policy_name = var.lambda_policy_name
+  source = "../modules/IAM/iam_lambda"
+  role_name= var.lambda_role_name
+  policy_name = var.lambda_policy_name
 }
 
 module "lambda_layer_requests"{
-    source ="../modules/Lambda/lambda-layer"
-    path = var.layers_path
-    layers_name = var.layers_list[0]
-    default_runtime = var.lambda_runtime
+  source ="../modules/Lambda/lambda-layer"
+  path = var.layers_path
+  layers_name = var.layers_list[0]
+  default_runtime = var.lambda_runtime
 
 }
 
 module "lambda_layer_kafkapython"{
-    source ="../modules/Lambda/lambda-layer"
-    path = var.layers_path
-    layers_name = var.layers_list[1]
-    default_runtime = var.lambda_runtime
+  source ="../modules/Lambda/lambda-layer"
+  path = var.layers_path
+  layers_name = var.layers_list[1]
+  default_runtime = var.lambda_runtime
 
 }
 module "lambda_producer_function_1"{
-    source = "../modules/Lambda/lambda-function"
+  source = "../modules/Lambda/lambda-function"
     
-    deployment_package  = "../../infrastructure/kafka/lambda-producer/packages/lambda-producer-deployment-package.zip"
-    name    = "producer_terraform_1"
-    arn =   module.IAM_lambda_role.role_arn
+  deployment_package  = "../../infrastructure/kafka/lambda-producer/packages/lambda-producer-deployment-package.zip"
+  name    = "producer_terraform_1"
+  arn =   module.IAM_lambda_role.role_arn
 
 
-    default_runtime = var.lambda_runtime[0]
-    execution_timeout   = var.lambda_timeout
-    layers_attaching = [module.lambda_layer_requests.layer_arn, module.lambda_layer_kafkapython.layer_arn]
-    producer_handler = var.lambda_producer_1_handler
+  default_runtime = var.lambda_runtime[0]
+  execution_timeout   = var.lambda_timeout
+  layers_attaching = [module.lambda_layer_requests.layer_arn, module.lambda_layer_kafkapython.layer_arn]
+  producer_handler = var.lambda_producer_1_handler
     
-    # Environment variable
-    servers  =   "${module.ec2_kafka_broker_instance.public_dns_hostname}:9093"
-    topics   =   var.kafka_topic
-    api_urls =  "https://api.coincap.io/v2/exchanges"
+  # Environment variable
+  servers  =   "${module.ec2_kafka_broker_instance.public_dns_hostname}:9093"
+  topics   =   var.kafka_topic
+  api_urls =  "https://api.coincap.io/v2/exchanges"
 
 }
 
@@ -162,9 +162,9 @@ module "eventbridge_trigger"{
 }
 
 resource "aws_lambda_permission" "allow_cloudwatch_to_call_check_foo" {
-    statement_id = "AllowExecutionFromCloudWatch"
-    action = "lambda:InvokeFunction"
-    function_name = module.lambda_producer_function_1.function_name
-    principal = "events.amazonaws.com"
-    source_arn = module.eventbridge_trigger.eventbridge_arn
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = module.lambda_producer_function_1.function_name
+  principal = "events.amazonaws.com"
+  source_arn = module.eventbridge_trigger.eventbridge_arn
 }
